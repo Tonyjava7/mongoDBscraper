3f33b59c383a40391ddbf76f2bfdeae3c7386076
Tony Chung <tonychung1976@gmail.com>
2018-05-09 23:41:09 -0400

    update server.js

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -2,9 +2,7 @@ function displayScrape() {
     $.getJSON("/scrape", function (scrape_code) {
         if (scrape_code.code == "success") {
             $.getJSON("/articles", function (data) {
-                $("#nyt-0").empty();
-                $("#nyt-1").empty();
-                $("#nyt-2").empty();
+                $("#nyt-0, #nyt-1, #nyt-2").empty();
                 $("#total-number").text(data.length);
                 for (var i = 0; i < data.length; i++) {
                     var mainDiv = $("<div>");
@@ -12,34 +10,22 @@ function displayScrape() {
                     var cardContentDiv = $("<div>");
                     cardContentDiv.addClass("card-content black-text");
                     var spanTitle = $("<span>");
-                    spanTitle.addClass("card-title");
-                    spanTitle.attr("data-id", data[i]._id);
-                    spanTitle.attr("id", "title-" + data[i]._id);
-                    spanTitle.text(data[i].title);
+                    spanTitle.addClass("card-title").attr("data-id", data[i]._id).attr("id", "title-" + data[i]._id).text(data[i].title);
                     var p = $("<p>");
                     p.text(data[i].summary);
                     p.attr("id", "summary-" + data[i]._id);
-                    cardContentDiv.append(spanTitle);
-                    cardContentDiv.append(p);
+                    cardContentDiv.append(spanTitle).append(p);
                     var cardActionDiv = $("<div>");
                     cardActionDiv.addClass("card-action");
                     var a = $("<a>");
-                    a.attr("href", data[i].link);
-                    a.attr("id", "link-" + data[i]._id);
-                    a.text("Go to the article");
+                    a.attr("href", data[i].link).attr("id", "link-" + data[i]._id).text("Go to the article");
                     cardActionDiv.append(a);
                     var saveArticle = $("<a>");
-                    saveArticle.addClass("waves-effect waves-light btn save-button");
-                    saveArticle.attr("id", data[i]._id);
-                    saveArticle.text("Save Article");
+                    saveArticle.addClass("waves-effect waves-light btn save-button").attr("id", data[i]._id).text("Save Article");
                     var byline = $("<p>");
-                    byline.text(data[i].byline);
-                    byline.attr("id", "byline-" + data[i]._id);
-                    cardActionDiv.append(byline);
-                    // cardActionDiv.append(button);
-                    cardActionDiv.append(saveArticle);
-                    mainDiv.append(cardContentDiv);
-                    mainDiv.append(cardActionDiv);
+                    byline.text(data[i].byline).attr("id", "byline-" + data[i]._id);
+                    cardActionDiv.append(byline).append(saveArticle);
+                    mainDiv.append(cardContentDiv).append(cardActionDiv);
                     $("#nyt-" + String(i % 3)).append(mainDiv);
                 }
             });
--- public/javascript/saved.js
+++ public/javascript/saved.js
@@ -1,48 +1,30 @@
 function displaySaved() {
     $.getJSON("/saved/all", function (data) {
-        $("#nyt-0").empty();
-        $("#nyt-1").empty();
-        $("#nyt-2").empty();
+        $("#nyt-0, #nyt-1, #nyt-2").empty();
         $("#total-number").text(data.length);
         for (var i = 0; i < data.length; i++) {
             var mainDiv = $("<div>");
-            mainDiv.addClass("card grey lighten-2");
-            mainDiv.attr("id", "main-" + data[i]._id);
+            mainDiv.addClass("card grey lighten-2").attr("id", "main-" + data[i]._id);
             var cardContentDiv = $("<div>");
             cardContentDiv.addClass("card-content black-text");
             var spanTitle = $("<span>");
-            spanTitle.addClass("card-title");
-            spanTitle.attr("data-id", data[i]._id);
-            spanTitle.attr("id", "title-" + data[i]._id);
-            spanTitle.text(data[i].title);
+            spanTitle.addClass("card-title").attr("data-id", data[i]._id).attr("id", "title-" + data[i]._id).text(data[i].title);
             var p = $("<p>");
-            p.text(data[i].summary);
-            p.attr("id", "summary-" + data[i]._id);
-            cardContentDiv.append(spanTitle);
-            cardContentDiv.append(p);
+            p.text(data[i].summary).attr("id", "summary-" + data[i]._id);
+            cardContentDiv.append(spanTitle).append(p);
             var cardActionDiv = $("<div>");
             cardActionDiv.addClass("card-action");
             var a = $("<a>");
-            a.attr("href", data[i].link);
-            a.attr("id", "link-" + data[i]._id);
-            a.text("Go to the article");
+            a.attr("href", data[i].link).attr("id", "link-" + data[i]._id).text("Go to the article");
             cardActionDiv.append(a);
             var button = $("<a>");
-            button.addClass("waves-effect waves-light white btn create-note modal-trigger");
-            button.attr("data-id", data[i]._id);
-            button.attr("data-target", "notes");
-            button.text("Create Notes");
+            button.addClass("waves-effect waves-light white btn create-note modal-trigger").attr("data-id", data[i]._id).attr("data-target", "notes").text("Create Notes");
             var deleteArticle = $("<a>");
-            deleteArticle.addClass("waves-effect waves-light white btn delete-button");
-            deleteArticle.attr("id", data[i]._id);
-            deleteArticle.text("Delete");
+            deleteArticle.addClass("waves-effect waves-light white btn delete-button").attr("id", data[i]._id).text("Delete");
             var byline = $("<p>");
             byline.text(data[i].byline);
-            cardActionDiv.append(byline);
-            cardActionDiv.append(button);
-            cardActionDiv.append(deleteArticle);
-            mainDiv.append(cardContentDiv);
-            mainDiv.append(cardActionDiv);
+            cardActionDiv.append(byline).append(button).append(deleteArticle);
+            mainDiv.append(cardContentDiv).append(cardActionDiv);
 
             $("#nyt-" + String(i % 3)).append(mainDiv);
         }
@@ -69,7 +51,7 @@ $(document).ready(function () {
     $(".button-collapse").sideNav();
     $('.modal').modal();
 
-    // When click on savearticle button
+
     $(document).on('click', '.save-button', function () {
         var thisId = $(this).attr("id");
         var summary = $("#summary-" + thisId).text();
@@ -93,7 +75,7 @@ $(document).ready(function () {
             }
         });
     });
-    // When click on delete article button
+
     $(document).on('click', '.delete-button', function () {
         var thisId = $(this).attr("id");
         var summary = $("#summary-" + thisId).text();
@@ -113,11 +95,10 @@ $(document).ready(function () {
         })
     });
 
-    // create note
+
     $(document).on("click", ".create-note", function (data) {
-        // alert($(this).attr("data-id"));
         $("#savenote").attr("data-id", $(this).attr("data-id"));
-        // <div id="display-note"></div>
+
         let aid = $(this).attr("data-id");
         let title = "Notes for the Article: " + aid;
         $("#display-title").empty();
@@ -162,14 +143,12 @@ $(document).ready(function () {
         });
     });
 
-    // When you click the savenote button
+
     $(document).on("click", "#savenote", function () {
-        // Grab the id associated with the article from the submit button
-        // get the user input value 
         var thisId = $(this).attr("data-id");
         var text = $("#textarea1").val();
         console.log(thisId);
-        // Run a POST request to change the note, using what's entered in the inputs
+    
         $.ajax({
             type: "POST",
             url: "/notes",
@@ -183,9 +162,9 @@ $(document).ready(function () {
             }
         });
     });
-    // delete note button
+
     $(document).on("click", "#deletenote", function () {
-        // Run a DELETE request to change the note, using what's entered in the inputs
+        
         $.ajax({
             type: "DELETE",
             url: "/deletenote",
